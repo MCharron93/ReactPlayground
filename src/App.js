b1608055fb305d9c1cbbbf5bf264dd8336fda83b
MCharron93 <michelle.charron93@gmail.com>
2021-02-11 09:47:03 -0700

    playground configure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Cards/Card'
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Here is where the Card should be inserting</p>
+      <Card />
     </div>
   );
 }
