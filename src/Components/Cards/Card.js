109d34bef37fcb1273ca4fba536e3dc83510da06
MCharron93 <michelle.charron93@gmail.com>
2021-02-11 11:32:11 -0700

    Linking in card anchor tag for IT requests

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -5,9 +5,13 @@ import './Card.css'
 function Card({title, description}){
     return(
         // NOTE since these are both properties, they will be injected into the JSX when the acutal .map() fires within the app rendering.
-        <div className="card-container">
+        <div>
+            <div className="card-container">
             <h1 className="heading-one">{title}</h1>
             <p className="description">{description}</p>
+            <br/>
+            <a href="https://form.asana.com/?k=qxP8ysQvIY6l5IDI4h9LUg&d=1195144362945755" target="_blank">Click me to launch a new tab</a>
+            </div>
         </div>
     )
 }
