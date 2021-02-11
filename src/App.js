cca4c35f2270020606a8cf30c6e56faa86c4c023
MCharron93 <michelle.charron93@gmail.com>
2021-02-11 10:01:56 -0700

    Notes for styling and similarities with Vue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+// NOTE import the components to be plugged in, similar to Vue when loading in a component to the page/vue
 import Card from './Components/Cards/Card'
 
 function App() {
   return (
     <div className="App">
+      {/* NOTE App is similar to root or App in Vue */}
       <header className="App-header">
         <p>
           This is the header
         </p>
       </header>
       <div className="body">
+        {/* NOTE custom styles can be inserted before the JSX element to controll for more customization with CSS */}
       <Card />
       </div>
     </div>
