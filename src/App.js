0fe977cbe648c747258a56cdfb073e4daf5fde81
MCharron93 <michelle.charron93@gmail.com>
2021-02-11 10:27:14 -0700

    Created mock card data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 import Card from './Components/Cards/Card'
 
 function App() {
+
+  const cards = [
+    {title: "New Blog Post",
+    description: "I have a very cute dog. I love to play ball with him"},
+    {title: "A Change in Plans",
+    description: "I recently was planning on going to the store, but plans have now changed due to a new game launching tonight. Playing that instead!"},
+    {title: "I Have a Book Club",
+    description: "New book club opening up this weekend! I hope everyone gets to join and read with us"},
+    {title: "Haven't Been Around for a While",
+    description: "I've been off the platform for a while now. I hope everyone is doing well. Been busy learning new code and stuff."},
+  ]
   return (
     <div className="App">
       {/* NOTE App is similar to root or App in Vue */}
@@ -11,9 +22,18 @@ function App() {
           This is the header
         </p>
       </header>
-      <div className="body">
         {/* NOTE custom styles can be inserted before the JSX element to controll for more customization with CSS */}
-      <Card />
+      <div className="body">
+        {/* NOTE This is where custome JSX components can be loaded in. They can have different classes and effects layered on top of them to become more dynamic.*/}
+        {/* NOTE In this case, one could use .map() to iterate over an array of Objs to render multiple cards  */}
+        {cards.map((card, index) => {
+          return(
+            <Card 
+            key={index}
+            title={card.title}
+            description={card.description}/>
+          )
+        })}
       </div>
     </div>
   );
