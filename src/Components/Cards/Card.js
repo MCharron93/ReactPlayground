423e8587370b0d896be12dafca9c36bfe3809f13
MCharron93 <michelle.charron93@gmail.com>
2021-02-11 11:34:48 -0700

    Added noreferrer for security in JSX

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -10,7 +10,7 @@ function Card({title, description}){
             <h1 className="heading-one">{title}</h1>
             <p className="description">{description}</p>
             <br/>
-            <a href="https://form.asana.com/?k=qxP8ysQvIY6l5IDI4h9LUg&d=1195144362945755" target="_blank">Click me to launch a new tab</a>
+            <a href="https://form.asana.com/?k=qxP8ysQvIY6l5IDI4h9LUg&d=1195144362945755" target="_blank" rel="noreferrer">Click me to launch a new tab</a>
             </div>
         </div>
     )
