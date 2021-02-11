ad3aba881aa554a1cd8c8b16a87c9bfcab8c04b0
MCharron93 <michelle.charron93@gmail.com>
2021-02-11 10:30:47 -0700

    Card edits to populate dummie data

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Card.css'
 
-function Card(){
+// NOTE similar to vue, we pass in the props of objs to the "class", in this case in the form of an obj with the properties/keys of title & description
+function Card({title, description}){
     return(
+        // NOTE since these are both properties, they will be injected into the JSX when the acutal .map() fires within the app rendering.
         <div className="card-container">
-            <h1 className="heading-one">Hello Fellow Nerds!</h1>
-            <p className="description">Here is some information that goes in the card. This is a short description of stuff in the card!</p>
+            <h1 className="heading-one">{title}</h1>
+            <p className="description">{description}</p>
         </div>
     )
 }
